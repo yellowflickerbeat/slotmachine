f9d5619c119946cf7c94d8dbdfcb215e649d8870
Harsha <160235068+yellowflickerbeat@users.noreply.github.com>
2024-05-07 01:15:22 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,33 @@
+MAX_LINES=3
+
+def entry():
+    amt = input("Enter the amount to be deposited:")
+    '''Normally, I would've just written:
+    amt=int(input(/'Enter the amount:'/))
+    However here we want to promt the user to enter a number above zero,
+     instead of just throwing an error'''
+    if amt.isdigit():
+        amt= int(amt)
+        if amt > 0:
+            break
+        else:
+            print("The amount should be more than zero.")
+    else:
+        print("Error.Enter a number.")
+    return amt
+
+def no_of_lines():
+    lines= input("Input numer of lines:")
+    if lines.isdigit():
+        lines= int(lines)
+        if 1 =< lines =< MAX_LINES:
+            break
+        else:
+            print("The number of lines should be valid.")
+    else:
+        print("Enter a number.")
+    return lines
+
+
+def main():
+    
